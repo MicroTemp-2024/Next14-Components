a7857e903cdcf0635196a43425fcf194d2d9a059
MicroTemp-2024 <k.net.game03@gmail.com>
2024-10-10 17:00:01 +0700

    Update CardContainer.tsx

--- next14components/src/app/components/CardContainer.tsx
+++ next14components/src/app/components/CardContainer.tsx
@@ -1,1 +1,20 @@
+import React from 'react';
 
+const CardContainer = ({ children }) => {
+  return (
+    <div className="bg-white shadow-md rounded px-8 pt-6 pb-4 mb-4">
+      <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
+        Card title
+      </h5>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+        {children}
+      </div>
+      <p className="text-gray-700 text-base mb-4">
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </p>
+    </div>
+  );
+};
+
+export default CardContainer;
